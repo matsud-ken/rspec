95ac60df00854758bb6e095d546157e208a153c6
matsuda-chitose <matsuda@chitose-kk.co.jp>
2025-04-01 14:48:48 +0900

    #createテストコード記述

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+    describe "POST /posts" do
+        let(:valid_params) { { post: { title: "Title", content: "Content" } } }
+
+        it "creates a Post" do
+            post posts_path, params: valid_params
+            expect(response).to have_http_status(:found)
+        end
+    end
+end
