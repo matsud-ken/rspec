481183c20e44a290bbd9a90fab3fd7d2579e417a
matsuda-chitose <matsuda@chitose-kk.co.jp>
2025-04-02 16:29:19 +0900

    #indexのテストコード追加

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
 
   gem 'rspec-rails', '~> 7.0.0'
   gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+
+    describe "GET #index" do
+        it "リクエストが成功すること" do
+            get posts_path
+            expect(response).to have_http_status(:ok)
+        end
+        it "Post一覧が取得できること" do
+            post = FactoryBot.create(:post)
+            get posts_path
+            expect(assigns(:posts)).to match_array(post)
+        end
+        it "indexページが表示されること" do
+            get posts_path
+            expect(response).to render_template :index
+        end
+    end
+
+    describe "GET #show" do
+        it "リクエストが成功すること" do
+            post = FactoryBot.create(:post)
+            get post_path(post)
+            expect(response).to have_http_status(:ok)
+        end
+        it "showページが表示されること" do
+            post = FactoryBot.create(:post)
+            get post_path(post)
+            expect(response).to render_template :show
+        end
     describe "POST #create" do
         let(:valid_params) { { post: { title: "Title", content: "Content" } } }
 
